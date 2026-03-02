a16082f1457ab46e7f5b5387520b5cd943ea84c9
Abhishek Awale <abhishekawale@Abhisheks-MacBook-Air.local>
2026-03-02 13:24:09 +0530

    express learning

---
+++ express-learn/app.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('first middleware executed!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('second middleware executed!');
+    res.send('<h1> Hello World! </h1>');
+});
+
+app.listen(3000, ()=>{
+    console.log('Server started on port 3000');
+});
